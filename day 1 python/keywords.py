4f7222500f04e95e82f4a540ae3434ba8dddec87
SanTiger <santosh.49347@gmail.com>
2024-02-08 18:43:20 +0530

    Day2 code

---
+++ day 1 python/keywords.py
@@ -0,0 +1,10 @@
+import keyword
+
+print("This is my first python programme")
+
+print(keyword.kwlist) # This will print all availabe keywords in python
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue',
+ 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in',
+ 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
